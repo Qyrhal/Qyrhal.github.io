73b35258f498ebb0fd2974bfac91cf2198105d04
Mid <kumarmid2005@gmail.com>
2023-03-12 10:25:06 +1100

    Update

--- README.md
+++ README.md
@@ -3,4 +3,8 @@ My webpage
 
 <hr>
 
-<b>Purpose:</b> This webpage is used to navigate through links and display useful information about me.
+<b>Purpose:</b> This webpage is used to navigate through links and display useful information about me and my projects.
+
+<hr>
+<b>Notes:<b>
+- all webpages are in different folders in order to remove ".html" at the end of each page for a cleaner look, this also functions as an organistation technique.
---
+++ freewatch/scripts.js
@@ -0,0 +1,47 @@
+$(function(){  // $(document).ready shorthand
+    $('.dictionarybox').fadeIn('slow');
+  });
+  
+  $(document).ready(function() {
+      
+      /* Every time the window is scrolled ... */
+      $(window).scroll( function(){
+      
+          /* Check the location of each desired element */
+          $('.fade').each( function(i){
+              
+              var bottom_of_object = $(this).position().top + $(this).outerHeight();
+              var bottom_of_window = $(window).scrollTop() + $(window).height();
+              
+              /* If the object is completely visible in the window, fade it it */
+              if( bottom_of_window > bottom_of_object ){
+                  
+                  $(this).animate({'opacity':'1'},1500);
+                      
+              }
+              
+          }); 
+      
+      });
+      
+});
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
